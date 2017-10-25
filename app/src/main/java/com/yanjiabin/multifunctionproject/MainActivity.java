fbd61f3439ac2b086c656fd4ad8d76621e3fa046
yanjiabin159357@163.com <yanjiabin159357@163.com>
2017-10-25 14:03:47 +0800

    旋转

--- app/src/main/java/com/yanjiabin/multifunctionproject/MainActivity.java
+++ app/src/main/java/com/yanjiabin/multifunctionproject/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,22 +18,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         img = (ImageView) findViewById(R.id.img);
 //        setAlpha();
-        setRotation();
+//        setRotation();
+        setScale();
+    }
+
+    private void setScale() {
+        ScaleAnimation scaleAnimation = new ScaleAnimation(0,8.0f,0,0.8f);
+        scaleAnimation.setDuration(2000);
+        scaleAnimation.setFillAfter(true);
+        img.setAnimation(scaleAnimation);
     }
 
     private void setRotation() {
         RotateAnimation rotateAnimation = new RotateAnimation(0, 360,  //从哪旋转，旋转多少度
-                Animation.RELATIVE_TO_SELF, 1.0f, Animation.RELATIVE_TO_SELF,
-                1.0f);
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
         rotateAnimation.setDuration(2000);
         rotateAnimation.setFillAfter(true);
+        rotateAnimation.setRepeatMode(Animation.REVERSE);
         img.startAnimation(rotateAnimation);
     }
 
     public void setAlpha(){
         AlphaAnimation alphaAnimation = new AlphaAnimation(0,0.8f);
         alphaAnimation.setDuration(2000);
-        alphaAnimation.setFillAfter(true);
         img.startAnimation(alphaAnimation);
     }
 }
